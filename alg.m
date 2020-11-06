8c5d6740aba5c23f11238b4f0d9516682ecb742e
Barbro34 <74043219+Barbro34@users.noreply.github.com>
2020-11-06 09:21:35 +0100

    Add files via upload

--- alg.m
+++ alg.m
@@ -2,7 +2,7 @@
 clear, clc
 
 % initilization
-theta = 0;
+theta = 2;
 VLSI_8_6_7;
 u = zeros(dimX*dimY*2);
 mex gsp.c;
@@ -16,13 +16,16 @@ for t = 1 : 1000
 
     % Calculate cost per route; remove route with
     % cost > 1 (required routes stored in nl and pairs in com)
+    h = sum(u);
     last = 0;
     for i = 1 : k
         first = last+1;
         slask = find(nl(last+1:length(nl)) == com(i,1));
         last = slask(1)+first-1;
         if (sum(pi(nl(first:last))) < 1)
-            okcom = [okcom i]; newnl = [newnl; nl(first:last)];
+            okcom = [okcom i]; 
+            newnl = [newnl; nl(first:last)];
+            h = h + 1 - sum(pi(nl(first:last)));
         end
     end
 
