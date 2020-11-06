2b1fa6fb06f6194573b7b5652fc3b000a4c8dca8
Barbro34 <74043219+Barbro34@users.noreply.github.com>
2020-11-06 11:52:28 +0100

    Add files via upload

--- alg.m
+++ alg.m
@@ -4,9 +4,11 @@ clear, clc
 % initilization
 theta = 2;
 VLSI_8_6_7;
-u = zeros(dimX*dimY*2,1000);
+u = ones(dimX*dimY*2,1001)./7;
 mex gsp.c;
 h = zeros(1000,1);
+alpha = zeros(1000,1);
+hs = k;
 
 % for loop
 for t = 1 : 1000
@@ -18,6 +20,8 @@ for t = 1 : 1000
     % Calculate cost per route; remove route with
     % cost > 1 (required routes stored in nl and pairs in com)
     h(t) = sum(pi);
+    okcom = [];
+    newnl = [];
     last = 0;
     for i = 1 : k
         first = last+1;
@@ -33,21 +37,39 @@ for t = 1 : 1000
     % calculate dir and lenght
     
     gamma = zeros(dimX*dimY*2,1);
+%     for i = 1 : dimX*dimY*2
+%         gamma(i) = 1;   
+%         last = 0;
+%         for l = 1 : length(okcom)
+%             first = last+1;
+%             slask = find(newnl(last+1:length(newnl)) == okcom(l));
+%             last = slask(1)+first-1;
+%             gamma(i) = gamma(i) - (length(newnl(first:last)) - 1);
+%         end
+%     end 
+    
     for i = 1 : dimX*dimY*2
         gamma(i) = 1;   
-        for l = okcom(1) : okcom(lenght(okcom))
-            gamma(i) = gamma(i) - (lenght(newnl(l,:)) - 1);
+        last = 0;
+        for l = 1 : length(okcom)
+            first = last+1;
+            slask = find(newnl(last+1:length(newnl)) == okcom(l));
+            last = slask(1)+first-1;
+            
+            if ismember(i,newnl(first:last)) == true
+                gamma(i) = gamma(i) - 1;
+            end
         end
     end 
-    
+
     % h* = k
-    alpha = theta*(k-h(t))/norm(gamma)^2;
+    alpha(t) = theta*(hs-h(t))/norm(gamma)^2;
     
     % update u and theta
 
-    u(:,t+1) = u(:,t) + gamma*alpha;
+    u(:,t+1) = u(:,t) + gamma*alpha(t);
     % projection
-    for i = 1 : lenght(u(:,t))
+    for i = 1 : length(u(:,t))
         if u(i,t+1) < 0
             u(i,t+1) = 0;
         end
@@ -59,3 +81,5 @@ for t = 1 : 1000
     
     % end for loop and update t
 end
+
+visagrind(dimX,dimY,newnl,okcom,pi,25);
